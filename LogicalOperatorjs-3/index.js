665ebceb7e3f74b718043661ffb046208cc7f537
ravi shankar <rvishnkr165@gmail.com>
2022-10-11 23:03:58 +0530

    logical operator added

---
+++ LogicalOperatorjs-3/index.js
@@ -0,0 +1,7 @@
+//Problem 3 : Given and character if it is a consonant print "Consonant"
+let char="r";
+if(char=="a"||char=="i"||char=="o"||char=="u"||char=="e"){
+  console.log("Vowel");
+}else{
+  console.log("Consonant");
+}
---
+++ LogicalOperatorjs-3/problem2.js
@@ -0,0 +1,7 @@
+//Problem 2 : Given any character, if it is a vowel print "Vowel"
+let char="i";
+if(char=="a"||char=="i"||char=="o"||char=="u"||char=="e"){
+  console.log("Vowel");
+}else{
+  console.log("Consonant");
+}
